f53403249f5aa7ca8f2335982970a325901f8c4f
Nathaniel-St-S <savoury.nathaniel@gmail.com>
2025-09-15 12:57:32 -0400

    i'm probably gonna need to make cache's structs

--- Projects/1/src/memory.c
+++ Projects/1/src/memory.c
@@ -8,27 +8,61 @@
 #define HEX_SIZE 5
 
 //Return the value at the given memory adress
-char read_mem(uword addr);
+uword read_mem(uword addr);
 
 //Write given value to the given adress, 
-void write_mem(uword addr, char* val);
+void write_mem(uword addr, uword val);
+
+//check if the given adress is in the given cache
+bool in_cache(uword* cache, int cache_size, uword addr);
 
 //Update the given cache in case of misses
-update_cache(char* cache, int cache_size, uword addr, char* val)
+void update_cache(uword* cache, int cache_size, uword addr, uword val);
+	
+//uword* L1 = (uword*)malloc(sizeof(uword) * L1CACHE_SIZE);
+//uword* L2 = (uword*)malloc(sizeof(uword) * L2CACHE_SIZE);
+//uword* RAM = (uword*)malloc(sizeof(uword) * RAM_SIZE)
+uword L1[L1CACHE_SIZE];
+uword L2[L2CACHE_SIZE];
+uword RAM[RAM_SIZE];
+
+//Track the numbur of misses for stats
+int L1cache_hit = 0, L1cache_miss = 0;
+int L2cache_hit = 0, L2cache_miss = 0;
 
 int main()
 {
-	//uword* l1_ptr = (uword*)malloc(sizeof(uword) * L1CACHE_SIZE);
-	//uword* l2_ptr = (uword*)malloc(sizeof(uword) * L2CACHE_SIZE);
-	//uword* ram_ptr = (uword*)malloc(sizeof(uword) * RAM_SIZE)
-	char l1[L1CACHE_SIZE][HEX_SIZE];
-	char l2[L2CACHE_SIZE][HEX_SIZE];
-	char ram[RAM_SIZE][HEX_SIZE];	
+	printf("read addr 5 <-> %s/n", read_mem(5));
+	printf("read addr 15 <-> %s/n", read_mem(15));
+	printf("read addr 5 <-> %s/n", read_mem(5));
+	printf("read addr 25 <-> %s/n", read_mem(25));
+	printf("read addr 15 <-> %s/n", read_mem(15));
+	
+	write_mem(5, 0xFFFF);
+	printf("read addr 5 <-> %s/n", read_mem(5));
+
+	printf("read addr 30 <-> %s/n", read_mem(30));
+	printf("read addr 60 <-> %s/n", read_mem(60));
+
+	return 0;
 }
 
-char read_mem(uword addr)
+bool in_cache(uword* cache, int cache_size, uword addr)
 {
-	if(in_cache(l1, L1CACHE_SIZE, addr))
+	for(int i = 0; i < cache_size; i++)
+	{
+		if(cache[i] == addr)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+uword read_mem(uword addr)
+{
+	if(in_cache(L1, L1CACHE_SIZE, addr))
 	{
 		//Cache hit at l1
 		L1cache_hit++;
@@ -36,9 +70,9 @@ char read_mem(uword addr)
 	}
 
 	//cache miss at L1
-	l1cache_miss++;
+	L1cache_miss++;
 
-	if(in_cache(l2, L2CACHE_SIZE, addr))
+	if(in_cache(L2, L2CACHE_SIZE, addr))
 	{
 		//Cache hit at l2
 		L2cache_hit++;
@@ -47,18 +81,18 @@ char read_mem(uword addr)
 		return L2[addr];
 	}
 	//Cache miss at L2
-	l2cache_miss++;
+	L2cache_miss++;
 
 	//Complete cache miss, so read RAM and update cache
-	char val = ram[addr];
+	uword val = RAM[addr];
 	update_cache(L1, L1CACHE_SIZE, addr, val);
-	update_Cache(L2, L2CACHE_SIZE, addr, val);
+	update_cache(L2, L2CACHE_SIZE, addr, val);
 	return val;
 }
 
-void write_memory(uword addr, const char* val)
+void write_memory(uword addr, const uword val)
 {
-	ram[addr] = val;
+	RAM[addr] = val;
 	
 	//Update L1 Cache
 	if(in_cache(L1, L1CACHE_SIZE, addr))
