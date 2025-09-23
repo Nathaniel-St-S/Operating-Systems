03ca60284011e2daf71fcb972175586e32c957ec
brysenPfingsten <brysen.pfingsten@gmail.com>
2025-09-22 23:46:50 -0400

    oops sorry for all the latex slop i'll get rid of it later

--- Projects/1/include/dma.h
+++ Projects/1/include/dma.h
@@ -4,8 +4,6 @@
 #include "types.h"
 
 //Starts the DMA transfer
-void initiateDMA(word* source, word destination, int size);
+void initiateDMA(word* source, word* destination, int size);
 
-//Transfers size amount of memory from source location to destination location
-void dmaTransfer(word* source, word destination, int size);
 #endif
